5e43e3cdf07eae5c36edc64bf1a1ac3802ab4908
Bhupesh Pradhan <76522149+HanakoK9@users.noreply.github.com>
2021-09-26 16:52:59 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div>
       <footer className='px-4 py-6 text-gray-300 bg-gray-800 flex flex-row justify-center'>
-        <div>Made with Next JS & Tailwind &copy; 2021</div>
+        <div>Made with Next & Tailwind &copy; 2021</div>
       </footer>
     </div>
   );
